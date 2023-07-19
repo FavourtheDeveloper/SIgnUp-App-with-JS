d18c3a1be994279851bf63506046d2f01590c7f6
FavourtheDev <favourthedeveloper@gmail.com>
2023-07-19 14:57:33 +0100

    Mini SignUp App with Javascriot

---
+++ app.js
@@ -0,0 +1,5 @@
+const submitbutton = document.getElementById("subbut");
+const userInput = document.getElementById("userinp");
+const userMail = document.getElementById("usermai");
+const alert = document.getElementById("errortext");
+
