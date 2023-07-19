af8a24d27385eb915735e5bf082ad681751a579b
FavourtheDev <favourthedeveloper@gmail.com>
2023-07-19 19:02:40 +0100

    Added some more features

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userMail = document.getElementById("usermai");
 const alert = document.getElementById("errortext");
 const userList = document.getElementById("users");
 
+document.getElementById("brand").style.color = "Yellow";
+
 submitbutton.addEventListener('click', onSubmit);
 
 function onSubmit(e) {
@@ -15,11 +17,12 @@ function onSubmit(e) {
         alert.innerHTML = "Please fill all fields"; 
     } else {
         let newLi = document.createElement("li");
-        newLi.innerHTML = userInput.value + " and E-Mail is " + userMail.value ;
+        newLi.innerHTML = "Name: " + userInput.value + "<br>E-Mail: " + userMail.value ;
         userList.appendChild(newLi);
         submitbutton.value = "REGISTERED";
+        document.body.style.backgroundColor = "Yellow";
         setTimeout(() => submitbutton.value = "Register", 3000);
-        
+        setTimeout(() => document.body.style.backgroundColor = "Brown", 3000);
         userInput.value = "";
         userMail.value = "";
     }
